e47bf2fb1a174a32c28bcb4d32bfb742c1ae4dea
Victoria Jaczynski <victoria.jacz.14@gmail.com>
2022-01-06 21:52:58 -0500

    final

--- js/script.js
+++ js/script.js
@@ -67,7 +67,7 @@ function submitButton(event)
 	{
           addingAh1.innerHTML = 'Enter your username: ';
 
-    		}
+    	}
 	else 
 	{
 		addingAh1.innerHTML = 'Incorrect email and/or password';
