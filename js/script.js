0110f5951188e2e1701c01ee9394bc1c1a62c900
Victoria Jaczynski <victoria.jacz.14@gmail.com>
2022-01-06 21:38:31 -0500

    comments

--- js/script.js
+++ js/script.js
@@ -1,18 +1,24 @@
 // Buttons
 
+// left button functions
 function leftButton() 
 {
 	let clickleftButton = doc.getElementById('container');
 	let leftButtonText = doc.createElement('section');
-	leftButtonText.innerText = "I'm right";
+
+	// what button will say when you click on it
+	leftButtonText.innerText = "I'm the right button";
 	clickleftButton.appendChild(leftButtonText);
 }
 
+// right button functions
 function rightButton() 
 {
 	let clickrightButton = doc.getElementById('container');
 	let rightButtonText = doc.createElement('section');
-	rightButtonText.innerHTML = "No, I'm right!";
+
+	// what button will say when you click on it
+	rightButtonText.innerHTML = "No, you're lying. I'm the real right button!";
 	clickrightButton.appendChild(rightButtonText);
 }
 
@@ -22,11 +28,14 @@ function rightButton()
 function onHover() 
 {
     let change = doc.createElement('section');
+	// what section will say when you hover over it
 	change.innerHTML = alert("Hey, I told you not to hover over me!");
 }
 
 window.onload = function() 
 {
+	// these are the actions that will trigger the buttons or phrase
+	// ex: clicking on the left button will trigger it to say "I'm the right button"
     doc.querySelector('#leftButton').addEventListener('click', leftButton);
     doc.querySelector('#rightButton').addEventListener('click', rightButton);
     doc.querySelector('#hover').addEventListener('mouseover', onHover);
@@ -40,12 +49,14 @@ function submitButton(event)
 {
 	event.preventDefault();
 
+	// putting in the values and storing
 	let underSubmitButton = doc.getElementById('');
 	let user = doc.getElementById('input1').value;
 	let pass = doc.getElementById('input2').value;
 	let addingAh1 = doc.createElement('h1');
 	underSubmitButton.appendChild(addingAH1);
 
+	// checking for valid email, user, and pass
 	for (var i = 0; i <= user.length; i++) 
 	{
 			if (parseFloat(user.length) && parseInt(pass) === 12345678)
