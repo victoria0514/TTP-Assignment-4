6fadf3f1fbef87c2d2fc209fe7240a15cdbb082d
Victoria Jaczynski <victoria.jacz.14@gmail.com>
2022-01-09 00:37:28 -0500

    final

--- script5.js
+++ script5.js
@@ -1,64 +1,51 @@
+// Button
+
 let text = document.getElementById("buttonText");
     function clickLeft(){
-
-            text.innerHTML = "Im right (left button)"
-
+            text.innerHTML = "I'm the right button!"
     }
 
     function clickRight(){
-
-        text.innerHTML = "no Im right (right button)"
+        text.innerHTML = "No, you're lying! I'm the right button!"
     }
 
- // Hovering over Button
-function onHover() 
-{
-    let change = doc.createElement('section');
-	// what section will say when you hover over it
-	change.innerHTML = alert("Hey, I told you not to hover over me!");
+
+// Hover
+
+function onHover(msg) {
+    alert("You're a bad listener...");
 }
 
-window.onload = function() 
-{
-	// these are the actions that will trigger the buttons or phrase
-	// ex: clicking on the left button will trigger it to say "I'm the right button"
-    doc.querySelector('#leftButton').addEventListener('click', leftButton);
-    doc.querySelector('#rightButton').addEventListener('click', rightButton);
-    doc.querySelector('#hover').addEventListener('mouseover', onHover);
-};
 
+// Submitting email, username, and password
 
-// Submit Button and Entering user, email, and pass
-function submitButton(event) 
-{
-	event.preventDefault();
+function final() {
+    let user = doc.forms["partThree"]["username"].value;
+    let email = doc.forms['partThree']["email"].value;
+    let pass = doc.forms["partThree"]["password"].value;
 
-	// putting in the values and storing
-	let underSubmitButton = doc.getElementById('');
-	let user = doc.getElementById('input1').value;
-	let pass = doc.getElementById('input2').value;
-	let addingAh1 = doc.createElement('h1');
-	underSubmitButton.appendChild(addingAH1);
+    if (user.length < 1) {
+        doc.getElementById('error-username').innerHTML = "!"
+    }
 
-	// checking for valid email, user, and pass
-	for (var i = 0; i <= user.length; i++) 
-	{
-		// if the password matches 12345678
-		if (parseFloat(user.length) && parseInt(pass) === 12345678)
-	{
+    if (pass.length < 1) {
+        doc.getElementById('error-password').innerHTML = "!";
+    }
 
-		addingAh1.innerHTML = 'Your information is correct!';
-	} 
-	// if you don't enter a username
-	else if(user.length == "")
-	{
-          addingAh1.innerHTML = 'Enter your username: ';
+    if (email.length < 3 ) {
+        doc.getElementById('error-email').innerHTML = "!";
+    }
 
-    	}
-	else 
-	{
-		addingAh1.innerHTML = 'Incorrect email and/or password';
-   	}
-        
-	}
+    if (pass != "12345678") {
+        alert("Try another password. Check your notes again.");
+        return false;
+    }
+
+    if (user.length < 1 || pass.length < 1) {
+        return false;
+    }
+
+    else {
+        alert("Good Job! You know your information.");
+    }
 }
